262e340b1743c6f0e708b291a7b5249a7bfb6ee7
KaramEisa <karam.eisa21@gmail.com>
2022-04-05 23:01:48 +0300

    added exercise 2.3 circle area calculation by radius and rounding up to two decimal places

---
+++ JAVASCRIPT/Functions/2.3-circle_area/app.js
@@ -0,0 +1,17 @@
+
+
+// 1- Write a function that calculates the circle area by a given radius as an argument.
+
+// Print the area as it is calculated and then print it rounded to two decimal places.
+
+
+
+
+
+function circleArea(radius) {
+    let area = Math.PI * (radius * radius);
+    console.log(area);
+    console.log(Math.round(area*100)/100);
+}
+ 
+circleArea(5);
