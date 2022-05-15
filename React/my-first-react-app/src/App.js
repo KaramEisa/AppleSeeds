a63970a0168007ddb9fa7e7fc3b35a923d6754c7
KaramEisa <karam.eisa21@gmail.com>
2022-05-15 14:19:05 +0300

    first react app
    added first react project

---
+++ React/my-first-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (<div>Hello World From App Component </div>
+    ); 
+  }
+}
+export default App;
